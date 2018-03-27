4aa4c3793e99f86be93106fb97968ab0d0ab0786
Md. Nur Alam Siddique <nur.alam.jewel72@gmail.com>
2018-03-27 17:08:33 +0600

    function

--- Python/function.py
+++ Python/function.py
@@ -1,6 +1,11 @@
-def add(n1,n2):
+'''def add(n1,n2):
     return n1+n2
 n=int(input('enter a number: '))
 m=int(input("enter another number: "))
 #result=add(n,m)
-print('your result is ',add(n,m))
+print('your result is ',add(n,m))'''
+def strchar(num):
+    num-=1
+    print(a[num])
+a='ahfdghdm'
+strchar(5)
