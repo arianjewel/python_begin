82e5507e26e21146d911ca51bb3546c6c6eef953
Md. Nur Alam Siddique <nur.alam.jewel72@gmail.com>
2018-03-29 22:41:44 +0600

    string

--- Python/string_with_turtle.py
+++ Python/string_with_turtle.py
@@ -1,10 +1,10 @@
 import turtle
 name=turtle.textinput('Name','What is your name?')
 name=name.lower()
-if name.startswith('mr'):
-    print('Hello sir, How are you?')
-elif name.startswith('mrs') or name.startswith('miss') or name.startswith('ms'):
+if name.startswith('mrs') or name.startswith('miss') or name.startswith('ms'):
     print('Hello madam, How are you?')
+elif name.startswith('mr'):
+    print('Hello sir, How are you?')
 else:
     name=name.capitalize()
     str='Hi '+name+ ' !, How are you?'
