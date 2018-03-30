8da1b14919af1a78d5f7d3e786fcd8488a57e3bc
Md. Nur Alam Siddique <nur.alam.jewel72@gmail.com>
2018-03-30 22:21:40 +0600

    set

---
+++ Python/set.py
@@ -0,0 +1,34 @@
+'''a=set()#empty set
+a={} #empty set
+a={1,2,3,4,5} #multi-set'''
+
+
+'''li=[1,2,3,4,5]
+tpl=(1,2,3,4)
+str='sri lanka'
+s=set(li)
+#now s={1,2,3,4,5}
+s1=set(tpl)
+#now s1={1,2,3,4}
+s2=set(str)
+#now s2={'s','r','i','','l','a','n','k','a'}'''
+
+
+
+'''a={1,2,3,4,5}
+b={2,4,6,8}
+c=a & b
+#now c={2,4}
+c1=a | b
+#now c1={1,2,3,4,5,6,8}
+c2=a ^ b
+#now c2={1,3,5,6,8}
+c3=a-b
+#now c3={1,3,5}
+c4=b-a
+#now c4={6,8}'''
+
+
+
+
+'''n.b: set element not in order'''
