737c3255be2ad2141406a01494bf3457ce8feef5
Md. Nur Alam Siddique <nur.alam.jewel72@gmail.com>
2018-03-30 21:36:17 +0600

    tuple test

---
+++ Python/tuple.py
@@ -0,0 +1,29 @@
+x=() #empty tuple
+x=1, #one element tuple
+x=(1,2,3) #multi-element tuple
+x=1,2,3 #multi-element tuple
+
+
+
+'''numbers=10,20,30,40
+n1,n2,n3,n4=numbers
+print(n1,n2,n3,n4)
+tup=n3,n4
+print(tup)
+#tuple assign in variable and variable assign in tuple'''
+
+
+
+
+'''items=1,2,5.5,['a','b','c'],('Apple','Mango'),'abc'
+for item in items:
+    print(item,type(item))
+#assign int,float,string,list,tuple in tuple'''
+
+
+
+'''tpl=['a','b','c'],(1,2,3)
+li=list(tpl)
+print(li)
+print(tpl[0][1],tpl[1][1])
+#tuple to list and index of tuple'''
