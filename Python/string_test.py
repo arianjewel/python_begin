bcba8f9cb8521400da3a2f73eb4a6dafc3c4584e
Md. Nur Alam Siddique <nur.alam.jewel72@gmail.com>
2018-05-05 20:44:47 +0600

    string

--- Python/string_test.py
+++ Python/string_test.py
@@ -1,15 +1,25 @@
 '''str='a quick brown fox jumps over the lazy dog'
 for c in'abcdefghijklmnopqrstuvwxyz':
     print(c,str.count(i))'''
+
+
+
 '''country='north Korea'
 new_country=country.replace('north','North')
 print(new_country)'''
+
+
+
 '''text='I am a programmer. I wanna be a good programmer. I wanna better. '
 new_text=text.replace('I','He')
 new_text2=new_text.replace('He am','He is')
 print(text)
 print(new_text)
 print(new_text2)'''
+
+
+
+
 '''str=input()
 capital='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 small='abcdefghijklmnopqrstuvwxyz'
@@ -31,6 +41,10 @@ print(up)
 print(lo)
 print(dig)
 print(ex)'''
+
+
+
+
 '''str=input()
 str=str.lower()
 new_str=''.join(reversed(str))
@@ -38,8 +52,11 @@ if new_str==str:
     print('Polindrome')
 else:
     print('Non Polindrome')'''
-str=list(input())
-n=len(str) 
+
+
+
+'''str=list(input())
+n=len(str)
 new_str=[0]*n
 for i in range(n):
     if i==len(str)-1 and i%2==0:
@@ -49,4 +66,22 @@ for i in range(n):
         new_str[i+1]=str[i]
     else:
         new_str[i-1]=str[i]
-print(''.join(new_str))
+print(''.join(new_str))'''
+
+
+
+
+def reverse(text):
+    r_text=''
+    index=len(text)-1
+    while index>=0:
+        r_text+=text[index]
+        index-=1
+    return r_text
+str=input('Please enter which you wanna compare between Polindrome and Non Polindrome: ')
+str=str.lower()
+new_str=reverse(str)
+if new_str==str:
+    print('Polindrome')
+else:
+    print('Non Polindrome')
